6d1ba0e01c3d6022980b118fdb0652e1b00ec8ba
talhashujahash <talha.shuja@alchemative.com>
2021-11-07 03:50:52 +0000

    change the credentials

--- src/Common/Utils.js
+++ src/Common/Utils.js
@@ -1,5 +1,5 @@
 export const shop = "";
 //export const baseUrl = "https://825a-119-73-120-105.ngrok.io";
 export const token = "Bearer e8ca54832038db60ede62e44827fc054eabfc2de";
-export const email = 'muhammadtalha';
-export const pwd = 'Peenu:)1';
+export const email = 'shopify-app-admin';
+export const pwd = 'Admin@1209';
